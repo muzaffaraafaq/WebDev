9f83ab0d4ba937bf6f5a7887d2039216e6fbc491
muzaffaraafaq <muzaffar.aafaq@gmail.com>
2022-09-12 17:10:59 -0400

    second commit

---
+++ Assignment1/Models/Rational.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1.Models
+{
+    public class Rational
+    {
+        public int Numerator { get; private set; }
+        public int Denominator { get; private set; }
+
+        public Rational(int numerator = 0, int denominator = 1)
+        {
+            this.Numerator = numerator;
+            this.Denominator = denominator;
+        }
+        public override string ToString()
+        {
+            return ($"{this.Numerator} / {this.Denominator}");
+        }
+        public void IncreaseBy(Rational other)
+        {
+            this.Numerator = this.Numerator * other.Denominator + other.Numerator * this.Denominator;
+            this.Denominator = this.Denominator * other.Denominator;
+        }
+        public void DecreaseBy(Rational other)
+        {
+            this.Numerator = this.Numerator * other.Denominator - other.Numerator * this.Denominator;
+            this.Denominator = this.Denominator * other.Denominator;
+        }
+    }
+}
---
+++ Assignment1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Assignment1.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Assignment1.Models
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rational rational1 = new Rational(0 , 2);
+            Rational rational2 = new Rational(1 , 2);
+            Rational rational3 = new Rational(2 , 2);
+            Rational rational4 = new Rational(2 , 1);
+
+            Console.WriteLine($"First Rational Number : {rational1}");
+            Console.WriteLine($"Second Rational Number : {rational2}");
+            Console.WriteLine("");
+            Console.WriteLine("Adding First Rational Number with Second Rational Number");
+            rational1.IncreaseBy(rational2);
+            Console.WriteLine("");
+            Console.WriteLine($"Increased by : {rational1}");
+            Console.WriteLine("");
+            Console.WriteLine($"Third Rational Number : {rational3}");
+            Console.WriteLine($"Forth Rational Number : {rational4}");
+            Console.WriteLine("");
+            Console.WriteLine("Adding Third Rational Number with Forth Rational Number");
+            rational3.DecreaseBy(rational4);
+            Console.WriteLine("");
+            Console.WriteLine($"Decreased by : {rational3}");
+            Console.ReadKey();
+            
+
+
+        }
+    }
+}
